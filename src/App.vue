b4c3b7a90b30438f848bb834af1ed0de2f5b4bd8
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-04 01:39:55 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
+    <Header></Header>
+    <div class="containre">
+    <SideMenu></SideMenu>
+    <Main></Main>
+    </div>
   </div>
 </template>
 
 <script>
+import 'normalize.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import SideMenu from './components/SideMenu';
 
 export default {
   name: 'App',
   components: {
+    Header,
+    Main,
+    SideMenu
   }
 }
 </script>
 
 <style>
 #app {
+  /* height: 1900px; */
+}
 
+.containre {
+  display: flex;
+  padding-top: 60px;
 }
 </style>
