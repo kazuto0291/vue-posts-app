e4facec9392eb8ecb5f11acc1c743d390245570d
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-04 01:39:40 +0900

    Header-com

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <h1 class="logo">Vue Hacks</h1>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+.header {
+  height: 60px;
+  position: fixed;
+  background-color: #3DAF7C;
+  width: 100%;
+  z-index: 1;
+}
+
+.logo {
+  margin: 0;
+  line-height: 60px;
+  color: #fff;
+  padding-left: 10px;
+}
+
+</style>
