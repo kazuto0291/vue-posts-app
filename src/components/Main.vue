b0ccb89d19b47688354714eab89d81beb48a4e2f
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-04 01:39:51 +0900

    Main-componet

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="main-container">
+    mainが入る
+  </main>
+</template>
+
+<script>
+export default {
+  name:'Main'
+}
+</script>
+
+<style>
+.main-container {
+  width: cal(100% - 200px);
+  flex: 1;
+  position: absolute;
+  left: 200px;
+  }
+</style>
