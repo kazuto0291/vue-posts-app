0d4f359ff9653431e4290acc49c5b065e349bc0c
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-10 19:34:16 +0900

    props-messageの設定

---
+++ src/components/Message.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="message">
+    <div class="message-date">{{ date }}</div>
+    <div class="message-text">{{ body }}</div>
+  </div>
+</template>
+
+<script scoped>
+export default {
+  props: {
+    body: {
+      type:String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+  },
+  name:'Message',
+}
+</script>
+
+<style scoped>
+  .message {
+    border-bottom: 1px solid black;
+    min-height: 80px;
+    padding: 10px;
+  }
+  .message:hover {
+    background-color: #eee;
+  }
+  .message-date {
+    font-size: 0.8;
+    color: #777;
+    text-align: right;
+  }
+
+  .message-text {
+    margin: 0;
+    margin-top: 5px;
+    white-space: preline;
+  }
+</style>
