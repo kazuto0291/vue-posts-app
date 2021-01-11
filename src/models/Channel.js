b435169198e7ffe3e109b2b077f6d713e1df58cd
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-11 22:10:31 +0900

    Channelsコレクション情報を取得する機能の実装
    Channnelインスタンスはid,nameプロパティを保持できるようなconstructorを実装する

---
+++ src/models/Channel.js
@@ -0,0 +1,33 @@
+import { from } from "core-js/fn/array";
+import { dbChannels } from '../db';
+import { firebase } from 'firebase';
+
+class Channel {
+  // constructor の引数をオブジェクトにすると順番を気にしなくてすむからおすすめ
+  constructor({id, name }) {
+    this.id = id;
+    this.name = name;
+  }
+
+  static async fetch() {
+    // 変数-collectionにdbChannelsからnameでソートした順で取得する
+    const collection = await dbChannels.orderBy('name').get();
+    if(collection.empty) {
+      return [];
+    }
+
+    return collection.docs.map(doc => {
+      return this.create(doc.id, doc.data())
+    });
+  }
+
+  static create(id, data) {
+    // Channelのインスタンスを作成する
+    return new Channel({
+      id,
+      name: data.name,
+    })
+  }
+}
+
+export default Channel;
